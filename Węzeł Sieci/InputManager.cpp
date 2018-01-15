34f520865c77caf119943907a83fa88e36717c70
Pawemol12 <Pawemol12@gmail.com>
2018-01-16 00:36:51 +0100

    Obsługa parametrów wejściowych

---
+++ Węzeł Sieci/InputManager.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+#include "InputManager.h"
+
+using namespace std;
+
+InputManager::InputManager()
+{ 
+}
+
+bool InputManager::isValidSwitcherKey(std::string switcherKey) 
+{
+	return (switcherKey == "-n" || switcherKey == "-t" || switcherKey == "-o");
+}
+
+bool InputManager::isSwitcherKey(std::string switcherKey)
+{
+	return switcherKey[0] == '-';
+}
+
+int InputManager::tryParseInputArguments(int argc, char* argv[]) 
+{
+	if (argc < TOTAL_PARAMETRS_NUMBER + 1)
+		return PARSE_INPUT_RESULT_NOT_ENAUGH_ARGUMENTS;
+
+	for (int i = 1; i < argc; i++) {
+		string switcherKey(argv[i]);
+		if (this->isValidSwitcherKey(switcherKey)) {
+			if (i + 1 > argc) {
+				cout << argc << endl;
+				return PARSE_INPUT_RESULT_NOT_PARAMETER_VALUE_AFTER_SWITCHER;
+			}
+
+			string switcherValue = argv[i + 1];
+			if (this->isSwitcherKey(switcherValue)) 
+			{
+				return PARSE_INPUT_SWITCHER_AFTER_SWITCHER;
+			}
+			else if (switcherKey == "-n") {
+				try {
+					this->timeBetweenPacketGeneration = stoi(switcherValue);
+				}
+				catch (const invalid_argument& ia) {
+					return PARSE_INPUT_RESULT_WRONG_VALUE;
+				}
+			}
+			else if (switcherKey == "-t") {
+				try {
+					this->simulationTotalTime = stoi(switcherValue);
+				}
+				catch (const invalid_argument& ia) {
+					return PARSE_INPUT_RESULT_WRONG_VALUE;
+				}
+			}
+			else if (switcherKey == "-o") {
+				this->outputFileName = switcherValue;
+			}
+
+		}
+
+	}
+}
+
+
+InputManager::~InputManager()
+{
+}
---
+++ Węzeł Sieci/InputManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#define TOTAL_PARAMETRS_NUMBER 3 
+
+enum ParseInputResult {
+	PARSE_INPUT_RESULT_NOT_ENAUGH_ARGUMENTS = -3,
+	PARSE_INPUT_RESULT_NOT_PARAMETER_VALUE_AFTER_SWITCHER,
+	PARSE_INPUT_SWITCHER_AFTER_SWITCHER,
+	PARSE_INPUT_RESULT_WRONG_VALUE,
+	PRASE_INPUT_RESULT_OK,
+};
+
+class InputManager
+{
+protected:
+	int timeBetweenPacketGeneration;
+	int simulationTotalTime;
+	std::string outputFileName;
+
+	bool isValidSwitcherKey(std::string switcherKey);
+	bool isSwitcherKey(std::string switcherKey);
+public:
+	InputManager();
+
+	int tryParseInputArguments(int argc, char* argv[]);
+
+	int getTimeBetweenPacketGeneration() const { return this->timeBetweenPacketGeneration; };
+	int getSimulationTotalTime() const { return this->simulationTotalTime; }
+	std::string getOutputFileName() const { return this->outputFileName; }
+
+	~InputManager();
+};
+
